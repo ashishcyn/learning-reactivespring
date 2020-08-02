9d48686a789ce88b6753e0caba6c33b5aee7ba6f
Ashish <c2nashish@gmail.com>
2020-08-02 23:19:15 +0400

    02.Created First Flux Stream end point

---
+++ src/main/java/com/ashi/tech/learningreactivespring/resources/FluxAndMonoController.java
@@ -0,0 +1,26 @@
+package com.ashi.tech.learningreactivespring.resources;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+@RestController
+public class FluxAndMonoController {
+
+    @GetMapping("/flux")
+    public Flux<Integer> returnFlux(){
+        return Flux.just(1,2,3,4)
+                .delayElements(Duration.ofSeconds(1))
+                .log();
+    }
+
+    @GetMapping(value = "/fluxstream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Integer> returnFluxStream(){
+        return Flux.just(1,2,3,4)
+                .delayElements(Duration.ofSeconds(1))
+                .log();
+    }
+}
