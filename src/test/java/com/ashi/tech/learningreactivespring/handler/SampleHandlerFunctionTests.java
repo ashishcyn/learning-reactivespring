ff3a38595c71de1323836f13bf57b915f1594d86
Ashish <c2nashish@gmail.com>
2020-08-03 17:30:17 +0400

    07.Adding Testcases for HandlerFunction

---
+++ src/test/java/com/ashi/tech/learningreactivespring/handler/SampleHandlerFunctionTests.java
@@ -0,0 +1,58 @@
+package com.ashi.tech.learningreactivespring.handler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class SampleHandlerFunctionTests {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void flux_approach1(){
+        Flux<Integer> integerFlux =  webTestClient.get()
+                .uri("/functional/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux)
+                .expectSubscription()
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void mono(){
+        Integer expectedValue = new Integer(1);
+
+        webTestClient.get().uri("/functional/mono")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Integer.class)
+                .consumeWith((response)->{
+                    assertEquals(expectedValue,response.getResponseBody());
+                });
+    }
+
+}
